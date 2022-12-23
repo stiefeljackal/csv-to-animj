a907bc4998714e0cde447004266698730bc17e64
Stiefel Jackal <stiefel@jackalworkz.net>
2022-12-23 01:56:14 -0600

    feat: add positional for results file name

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ import { convertToAnimjInFolder } from './src/convert-csv-to-animj.js'
 const argv = yargs(process.argv.slice(2))
 
 argv
-.usage('$0 <cmd> [args]')
-.command('convert [folderPath]', 'Convert CSV to AnimJ in folder.', (yargs) => {
-  yargs.positional('folderPath', {
-    type: 'string',
-    describe: 'The file path to convert the files'
-  })
-}, (argv) => convertToAnimjInFolder(argv.folderPath)
-).help()
-.argv
+  .usage('$0 <cmd> [args]')
+  .command('convert [folderPath] [resultFileName]', 'Convert CSV to AnimJ in folder.', (yargs) => {
+    yargs.positional('folderPath', {
+      type: 'string',
+      describe: 'The file path to convert the files'
+    })
+      .positional('resultFileName', {
+        type: 'string',
+        describe: 'The name of the file that the AnimJ file should have'
+      })
+  }, (argv) => convertToAnimjInFolder(argv.folderPath, argv.resultFileName)
+  ).help()
+  .argv
